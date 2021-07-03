b6de6b441675626cd4ed5b9de921841c87e17fbf
yasmeen46 <86551306+yasmeen46@users.noreply.github.com>
2021-07-04 00:27:27 +0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		
 		public String getMessage(boolean bigger) {
 					if (bigger) {
-									return "Hello Universe!";
+									return "Hello Universe11!";
 											} else {
 															return "Hello World, Hello, Hello!";											}
 						}
